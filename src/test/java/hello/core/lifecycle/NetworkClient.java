4dece84d36441e6b5f51eb8a9ad55123c8101443
taehyun9244 <85472257+taehyun9244@users.noreply.github.com>
2022-03-13 23:25:32 +0900

    initMethod = "init", destroyMethod = "close"

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+
+
+public class NetworkClient  {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url =" + url);
-        connect();
-        call("초기화 연결 메세지");
+
     }
     public void setUrl(String url){
         this.url = url;
@@ -24,5 +25,17 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect(){
         System.out.println("close" + url);
+    }
+
+
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
